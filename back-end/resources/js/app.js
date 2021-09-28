1b77a268740b059662b18521bdb9c19acc2a47eb
Darin Jan <darinjan13@gmail.com>
2021-09-28 20:52:31 +0800

    Vuetify

--- back-end/resources/js/app.js
+++ back-end/resources/js/app.js
@@ -8,6 +8,8 @@ require('./bootstrap');
 
 window.Vue = require('vue').default;
 
+import vuetify from '../plugins/vuetify'
+
 /**
  * The following block of code may be used to automatically register your
  * Vue components. It will recursively scan this directory for the Vue
@@ -28,5 +30,6 @@ Vue.component('yawa', require('./components/Yawa.vue').default);
  */
 
 const app = new Vue({
+    vuetify,
     el: '#app',
 });
--- back-end/resources/js/components/Yawa.vue
+++ back-end/resources/js/components/Yawa.vue
@@ -1,5 +1,13 @@
-<template >
-    <div>
-        YAWA
-    </div>
+<template>
+    <v-app>
+    <div class="text-center">
+    <v-btn
+      rounded
+      color="primary"
+      dark
+    >
+      Rounded Button
+    </v-btn>
+  </div>    
+    </v-app>
 </template>
